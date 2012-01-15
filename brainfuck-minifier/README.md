56b126d60b6c4afda7ded7ad7ac720b82010a229
Mathias Bynens <mathias@qiwi.be>
2012-01-15 13:59:23 +0100

    Add brainfuck minifier

---
+++ brainfuck-minifier/README.md
@@ -0,0 +1,7 @@
+# [Mothereffing brainfuck minifier](http://mothereff.in/brainfuck-minifier)
+
+[Brainfuck](http://esolangs.org/wiki/Brainfuck) _(not capitalized except at the start of a sentence)_ is an esoteric programming language. It has eight commands — `+-<>[],.` — and ignores all other characters. As a result, no special syntax for comments is needed, as long as the comments don’t contain the command characters.
+
+This tool will automatically remove comments from any brainfuck snippet you enter.
+
+Made by [Mathias Bynens](http://mathiasbynens.be/).
---
+++ brainfuck-minifier/eff.js
@@ -0,0 +1,111 @@
+(function(window, document) {
+
+	var textarea = document.getElementsByTagName('textarea')[0],
+	    characters = document.getElementById('characters'),
+	    pre = document.getElementsByTagName('pre')[0],
+	    output = document.getElementById('output'),
+	    permalink = document.getElementById('permalink'),
+	    dds = document.getElementsByTagName('dd'),
+	    before = dds[0],
+	    after = dds[1],
+	    ratio = dds[2],
+	    regexNotBrainfuck = /[^\+\-<>\[\],\.]/g,
+	    regexNumberGroup = /(?=(?:\d{3})+$)(?!\b)/g,
+	    // http://mathiasbynens.be/notes/localstorage-pattern
+	    storage = (function() {
+	    	try {
+	    		var storage = window.localStorage,
+	    		    uid = new Date;
+	    		storage.setItem(uid, uid);
+	    		return storage.getItem(uid) == uid && storage;
+	    	} catch(e) {}
+	    }()),
+	    characterReferences;
+
+	function text(el, str) {
+		if (str == null) {
+			return el.innerText || el.textContent;
+		}
+		el.innerText != null && (el.innerText = str);
+		el.textContent != null && (el.textContent = str);
+	}
+
+	// Taken from http://mths.be/punycode
+	function ucs2decode(string) {
+		var output = [],
+		    counter = 0,
+		    length = string.length,
+		    value,
+		    extra;
+		while (counter < length) {
+			value = string.charCodeAt(counter++);
+			if ((value & 0xF800) == 0xD800) {
+				extra = string.charCodeAt(counter++);
+				if ((value & 0xFC00) != 0xD800 || (extra & 0xFC00) != 0xDC00) {
+					throw Error('Illegal UCS-2 sequence');
+				}
+				value = ((value & 0x3FF) << 10) + (extra & 0x3FF) + 0x10000;
+			}
+			output.push(value);
+		}
+		return output;
+	}
+
+	function formatNumber(number, unit) {
+		return (number == 0 ? '0' : String(number).replace(regexNumberGroup, ',')) + ' ' + unit + (number == 1 ? '' : 's');
+	}
+
+	function charCount(string) {
+		return ucs2decode(string).length;
+	}
+
+	function byteCount(string) {
+		return ~-encodeURI(string).split(/%..|./).length; // https://gist.github.com/1010324
+	}
+
+	function update() {
+		var value = textarea.value,
+		    result = value.replace(regexNotBrainfuck, ''),
+		    originalCharacterCount = charCount(value),
+		    originalByteCount = byteCount(value),
+		    resultingByteCount = byteCount(result); // byte and character counts are the same at this point
+		text(output, result || '[no output]');
+		pre.className = resultingByteCount ? '' : 'fail';
+		text(before, formatNumber(originalByteCount, 'byte') + (originalCharacterCount == originalByteCount ? '' : ' (assuming UTF-8); ' + formatNumber(originalCharacterCount, 'character')));
+		text(after, formatNumber(resultingByteCount, 'byte'));
+		text(ratio, (originalByteCount ? ((originalByteCount - resultingByteCount) / originalByteCount * 100) : 0).toFixed(2) + '%');
+
+		permalink.hash = encodeURIComponent(value);
+		storage && (storage.brainfuck = value);
+	};
+
+	textarea.onkeyup = update;
+	textarea.oninput = function() {
+		textarea.onkeyup = null;
+		update();
+	};
+
+	if (storage) {
+		storage.brainfuck && (textarea.value = storage.brainfuck);
+		update();
+	}
+
+	window.onhashchange = function() {
+		textarea.value = decodeURIComponent(location.hash.slice(1));
+		update();
+	};
+
+	if (location.hash) {
+		window.onhashchange();
+	}
+
+}(this, document));
+
+// Google Analytics
+window._gaq = [['_setAccount', 'UA-6065217-60'], ['_trackPageview']];
+(function(d, t) {
+	var g = d.createElement(t),
+	    s = d.getElementsByTagName(t)[0];
+	g.src = '//www.google-analytics.com/ga.js';
+	s.parentNode.insertBefore(g, s);
+}(document, 'script'));
