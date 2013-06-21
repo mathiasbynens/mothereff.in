8e1178dbaec5708509b09115383b79e29fa8224e
Mathias Bynens <mathias@qiwi.be>
2013-06-21 12:56:20 +0200

    lua-minifier: Update dependencies

--- lua-minifier/vendor/update.sh
+++ lua-minifier/vendor/update.sh
@@ -2,7 +2,7 @@
 
 cd "$(dirname "${BASH_SOURCE}")"
 
-curl -# "https://raw.github.com/oxyc/luaparse/master/lib/luaparse.js" > luaparse.js
+curl -# "https://raw.github.com/oxyc/luaparse/master/luaparse.js" > luaparse.js
 curl -# "https://raw.github.com/mathiasbynens/luamin/master/luamin.js" > luamin.js
 
 cat "luaparse.js" "luamin.js" "../eff.js" > "/tmp/lua-minifier.js"
