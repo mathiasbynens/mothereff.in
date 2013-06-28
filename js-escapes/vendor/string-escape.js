880f773b51200fc901d9159ad5fa369d440b6cae
Mathias Bynens <mathias@qiwi.be>
2013-06-28 23:46:27 +0200

    Update dependencies

--- js-escapes/vendor/string-escape.js
+++ js-escapes/vendor/string-escape.js
@@ -1,4 +1,4 @@
-/*! http://mths.be/stringescape v0.1.6 by @mathias */
+/*! http://mths.be/stringescape v0.2.2 by @mathias */
 ;(function(root) {
 
 	// Detect free variables `exports`
@@ -19,59 +19,119 @@
 
 	var object = {};
 	var hasOwnProperty = object.hasOwnProperty;
-	var hasKey = function(object, key) {
-		return hasOwnProperty.call(object, key);
+	var forOwn = function(object, callback) {
+		var key;
+		for (key in object) {
+			if (hasOwnProperty.call(object, key)) {
+				callback(key, object[key]);
+			}
+		}
 	};
 
 	var extend = function(destination, source) {
 		if (!source) {
 			return destination;
 		}
-		var key;
-		for (key in source) {
-			if (hasOwnProperty.call(source, key)) {
-				destination[key] = source[key];
-			}
-		}
+		forOwn(source, function(key, value) {
+			destination[key] = value;
+		});
 		return destination;
+	};
+
+	var forEach = function(array, callback) {
+		var length = array.length;
+		var index = -1;
+		while (++index < length) {
+			callback(array[index]);
+		}
+	};
+
+	var toString = object.toString;
+	var isArray = function(value) {
+		return toString.call(value) == '[object Array]';
+	};
+	var isString = function(value) {
+		return typeof value == 'string' ||
+			toString.call(value) == '[object String]';
 	};
 
 	/*--------------------------------------------------------------------------*/
 
-	var cache = {
-		// http://es5.github.com/x7.html#x7.8.4
-		// Table 4 — String Single Character Escape Sequences
-		'\b': '\\b',
-		'\t': '\\t',
-		'\n': '\\n',
-		'\v': '\\x0B', // In IE < 9, '\v' == 'v'
-		'\f': '\\f',
-		'\r': '\\r',
-		'\\': '\\\\',
+	// http://mathiasbynens.be/notes/javascript-escapes#single
+	var singleEscapes = {
 		'"': '\\"',
-		'\'': '\\\''
+		'\'': '\\\'',
+		'\\': '\\\\',
+		'\b': '\\b',
+		'\f': '\\f',
+		'\n': '\\n',
+		'\r': '\\r',
+		'\t': '\\t'
+		// `\v` is omitted intentionally, because in IE < 9, '\v' == 'v'
+		// '\v': '\\x0B'
 	};
+	var regexSingleEscape = /["'\\\b\f\n\r\t]/;
 
 	var regexDigit = /[0-9]/;
 	var regexWhitelist = /[\x20\x21\x23-\x26\x28-\x5B\x5D-\x7E]/;
 
-	var stringEscape = function(string, options) {
+	var stringEscape = function(argument, options) {
 		// Handle options
 		var defaults = {
 			'escapeEverything': false,
 			'quotes': 'single',
-			'wrap': false
+			'wrap': false,
+			'compact': true,
+			'indent': '\t'
 		};
 		options = extend(defaults, options);
 		if (options.quotes != 'single' && options.quotes != 'double') {
 			options.quotes = 'single';
 		}
+		var json = options.json;
+		if (json) {
+			options.quotes = 'double';
+			options.wrap = true;
+		}
 		var quote = options.quotes == 'double' ? '"' : '\'';
+		var compact = options.compact;
+		var indent = options.indent;
+		var newLine = compact ? '' : '\n';
+		var result;
 
+		if (!isString(argument)) {
+			if (isArray(argument)) {
+				// assume it’s a flat array with only string values
+				result = [];
+				options.wrap = true;
+				forEach(argument, function(value) {
+					result.push(
+						(compact ? '' : indent) +
+						stringEscape(value, options)
+					);
+				});
+				return '[' + newLine + result.join(',' + newLine) + newLine + ']';
+			} else {
+				// assume it’s a flat object with only string values
+				result = [];
+				options.wrap = true;
+				forOwn(argument, function(key, value) {
+					result.push(
+						(compact ? '' : indent) +
+						stringEscape(key, options) + ':' +
+						(compact ? '' : ' ') +
+						stringEscape(value, options)
+					);
+				});
+				return '{' + newLine + result.join(',' + newLine) + newLine + '}';
+			}
+		}
+
+		var string = argument;
 		// Loop over each code unit in the string and escape it
 		var index = -1;
 		var length = string.length;
-		var result = '';
+		result = '';
 		while (++index < length) {
 			var character = string.charAt(index);
 			if (!options.escapeEverything) {
@@ -90,19 +150,25 @@
 					continue;
 				}
 			}
-			if (character == '\0' && !regexDigit.test(string.charAt(index + 1))) {
+			if (
+				character == '\0' &&
+				!json &&
+				!regexDigit.test(string.charAt(index + 1))
+			) {
 				result += '\\0';
 				continue;
 			}
-			if (hasKey(cache, character)) {
-				result += cache[character];
+			if (regexSingleEscape.test(character)) {
+				// no need for a `hasOwnProperty` check here
+				result += singleEscapes[character];
 				continue;
 			}
 			var charCode = character.charCodeAt(0);
 			var hexadecimal = charCode.toString(16).toUpperCase();
-			var longhand = hexadecimal.length > 2;
-			result += cache[character] = '\\' + (longhand ? 'u' : 'x') +
+			var longhand = hexadecimal.length > 2 || json;
+			var escaped = '\\' + (longhand ? 'u' : 'x') +
 				('0000' + hexadecimal).slice(longhand ? -4 : -2);
+			result += escaped;
 			continue;
 		}
 		if (options.wrap) {
@@ -111,7 +177,7 @@
 		return result;
 	};
 
-	stringEscape.version = '0.1.6';
+	stringEscape.version = '0.2.2';
 
 	/*--------------------------------------------------------------------------*/
 
