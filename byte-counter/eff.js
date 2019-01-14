afe044d296af5429b4e7871bc60cdb551f2fa982
Mathias Bynens <mathias@qiwi.be>
2019-01-14 10:10:28 +0100

    Simplify byte-counter using modern language features and techniques

--- byte-counter/eff.js
+++ byte-counter/eff.js
@@ -18,33 +18,6 @@
 		} catch (exception) {}
 	}());
 
-	// Taken from https://mths.be/punycode
-	function ucs2decode(string) {
-		var output = [],
-		    counter = 0,
-		    length = string.length,
-		    value,
-		    extra;
-		while (counter < length) {
-			value = string.charCodeAt(counter++);
-			if (value >= 0xD800 && value <= 0xDBFF && counter < length) {
-				// high surrogate, and there is a next character
-				extra = string.charCodeAt(counter++);
-				if ((extra & 0xFC00) == 0xDC00) { // low surrogate
-					output.push(((value & 0x3FF) << 10) + (extra & 0x3FF) + 0x10000);
-				} else {
-					// unmatched surrogate; only append this code unit, in case the next
-					// code unit is the high surrogate of a surrogate pair
-					output.push(value);
-					counter--;
-				}
-			} else {
-				output.push(value);
-			}
-		}
-		return output;
-	}
-
 	function encode(string) {
 		// URL-encode some more characters to avoid issues when using permalink URLs in Markdown
 		return encodeURIComponent(string).replace(/['()_*]/g, function(character) {
@@ -59,8 +32,8 @@
 	function update() {
 		var value = textarea.value.replace(/\r\n/g, '\n'),
 		    encodedValue = encode(value),
-		    byteCount = utf8.encode(value).length, // https://mths.be/utf8js
-		    characterCount = ucs2decode(value).length;
+		    byteCount = new Blob([value]).size,
+		    characterCount = [...value].length;
 		characters.innerHTML = formatNumber(characterCount, 'character');
 		bytes.innerHTML = formatNumber(byteCount, 'byte');
 		permalink.hash = encodedValue;
